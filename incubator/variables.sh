4610629eb168e2459ec22aa220b1e1752ea408ed
ttrehan <trehan.tarun@gmail.com>
2018-03-18 10:34:18 +0530

    Shell script variables

--- incubator/variables.sh
+++ incubator/variables.sh
@@ -2,4 +2,10 @@
 
 echo "Declaring variables in shell."
 
+me=$(whoami)
+echo "Current user is:${me}"
+
+#use export to delcare variables aviable to shell enviornment of subsequently executing commands
+export currUserName=$(whoami)
+echo "Current user in exported variable is:${currUserName}"
 
