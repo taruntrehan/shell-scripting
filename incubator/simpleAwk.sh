1c88e43090000b454968a68f7c4b71ba0f713f7b
ttrehan <trehan.tarun@gmail.com>
2018-03-25 10:55:34 +0530

    Awk Samples Added.

--- incubator/simpleAwk.sh
+++ incubator/simpleAwk.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-echo "Hello to shell-scripting."
+echo "Hello to awk-scripting."
+
+#General syntax of an awk command
+#awk 'BEGIN {start_action} {action} END {stop_action}' filename
+
+# The input file we've create is an output of ls -lrt , something like a table with rows and columns
+
+awk '{print $1}' awkSampleData.txt
+
+awk 'BEGIN {sum=0} {sum=sum+$5} END {print sum}' awkSampleData.txt
+
+awk '{ if($9 == "simpleSed.sh") print $0;}' awkSampleData.txt
+
+ awk 'BEGIN { for(i=1;i<=5;i++) print "square of", i, "is",i*i; }'
