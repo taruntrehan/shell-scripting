5f261f61e3491d563ffa388d3479fc19aeed7d28
ttrehan <trehan.tarun@gmail.com>
2018-05-15 07:36:03 +0530

    Error Handling and Curl Utils

--- incubator/curlUtils.sh
+++ incubator/curlUtils.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-echo "Hello to shell-scripting."
+echo "Hello to curl command."
+# Call a url and get response
+curl http://ip.jsontest.com/?callback=showMyIP > curlResponse.txt
--- incubator/errorHandling.sh
+++ incubator/errorHandling.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
 echo "Hello to shell-scripting."
+export ERROR_CODE="0"
+ERROR_CODE=$?
+export NAME="Tarun"
+
+cd /directory/does/not/exist
+
+ERROR_CODE=$?
+if [ ${ERROR_CODE} -ne 0 ]; then
+	echo "Some error has occured in shell script."
+	exit 1
+fi
+
+exit 0
+
