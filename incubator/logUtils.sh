4fd83c121b936a18d1e936627b2ee3b237514de8
ttrehan <trehan.tarun@gmail.com>
2018-06-09 12:27:07 +0530

    Mail Utils added.Logging example in next commit.

--- incubator/logUtils.sh
+++ incubator/logUtils.sh
@@ -2,3 +2,5 @@
 
 echo "Hello to shell-scripting."
 
+#This program is written to enable simple logging in shell scripts
+
--- incubator/mailUtils.sh
+++ incubator/mailUtils.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-echo "Hello to shell-scripting."
+echo "Hello to mail operations via shell-scripting."
+
+#You need to configure mail command prior to install
+#You admin can help you with the same.
+
+echo "Mail body" | mail -s "Mail subject" trehan.tarun@gmail.com
+
+#In case you have body in a text file, use the following:
+
+cat bodyTextInput.log | mail -s "Mail subject" trehan.tarun@gmail.com
+
+# Sending a file as attachment and body as textfile.
+
+uuencode attachment.zip | mail -s "Mail subject" "trehan.tarun@gmail.com" < bodyTextInput.log
